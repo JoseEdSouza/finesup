b64e2433852de7389612a62d350b6f561e969856
José Souza <joseedsouza.dev@gmail.com>
2023-10-11 12:52:37 -0300

    started database class and its settings

--- api/src/config/settings.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class DBSettings(Enum):
-    HOST = 'localhost'
-    DBNAME = 'finesup'
-    USER = 'postgres'
-    PASSWORD = 'Jose24122003'
-    URL = f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}/{DBNAME}'
