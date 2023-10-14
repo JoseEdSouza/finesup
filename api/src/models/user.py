430f83a606bcdb77b47f81307e2d56651a0df317
PHS-7 <moreira.phs@alu.ufc.br>
2023-10-14 14:35:03 -0300

    user class modeling

---
+++ api/src/models/user.py
@@ -0,0 +1,15 @@
+class User:
+
+    def __init__(self, user_id: int | None, name:str, email: str, password: str):
+        self.__user_id = user_id
+        self.name = name
+        self.email = email
+        self.password = password
+
+    @property
+    def user_id(self) -> int:
+        return self.__user_id
+
+    def __str__(self):
+        return f' User (ID: {self.__user_id}, Name: {self.name}, Email: {self.email}, Password: {self.password})'
+
---
+++ api/src/models/userDAO.py
@@ -0,0 +1,109 @@
+import psycopg2 as pg
+from abc import ABC, abstractmethod
+from api.src.models.user import User
+from api.src.db.database import Database
+
+
+class UserDAO(ABC):
+
+    @abstractmethod
+    def add(self, user: User) -> bool:
+        pass
+
+    @abstractmethod
+    def update(self, user_id: int, user: User)->bool:
+        pass
+
+    @abstractmethod
+    def remove(self, user_id: int)->bool:
+        pass
+
+    @abstractmethod
+    def get(self, user_id: int) ->User | None:
+        pass
+
+
+class UserDAOImp(UserDAO):
+    __conn = None
+    __cursor = None
+
+    def __init__(self, ):
+        db = Database()
+        self.__conn = db.connection
+        self.__cursor = self.__conn.cursor()
+
+    def __save(self):
+        self.__conn.commit()
+
+    def add(self, user: User) -> bool:
+        values = (user.name, user.email, user.password)
+        try:
+            self.__cursor.execute('''
+            INSERT INTO users (name, email, password, creation_date) 
+            VALUES (%s, %s, MD5(%s), now())
+            ''', values)
+
+        except pg.Error as e:
+            print(e)
+            return False
+
+        finally:
+            self.__save()
+            return True
+
+    def update(self, user_id: int, user: User) -> bool:
+        values = (user.name, user.email, user.password, user_id)
+
+        try:
+            self.__cursor.execute('''
+            UPDATE users SET name = %s, email = %s, password = MD5(%s) WHERE id = %s
+            ''', values)
+
+        except pg.Error as e:
+            print(e)
+            return False
+
+        finally:
+            self.__save()
+            return True
+
+    def remove(self, user_id: int) -> bool:
+        try:
+            self.__cursor.execute('''
+            DELETE FROM users WHERE id = %s
+            ''', (user_id,))
+
+        except pg.Error as e:
+            print(e)
+            return False
+
+        finally:
+            self.__save()
+            return True
+
+    def get(self, user_id: int) -> User | None:
+        try:
+            self.__cursor.execute('''
+            SELECT * FROM users WHERE id = %s
+            ''', (user_id,))
+            # id | name | email | password | creation_date
+            result = self.__cursor.fetchone()
+            if result is None:
+                return None
+            else:
+                return User(
+                    user_id=result[0],
+                    name=result[1],
+                    email=result[2],
+                    password=result[3]
+                )
+        except pg.Error as e:
+            print(e)
+            return None
+
+if __name__ == '__main__':
+    x = UserDAOImp()
+    # a = User(None, 'mateus', 'matues@mail.com', 'senha123')
+    # print(x.get(3))
+    # x.remove(3)
+    print(x.get(2))
