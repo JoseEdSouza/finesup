b234fcf7eed8d197672fff25f0ccc1afade1b65d
José Souza <joseedsouza.dev@gmail.com>
2023-10-15 20:55:43 -0300

    fixed transaction models constructor

--- api/src/models/transaction.py
+++ api/src/models/transaction.py
@@ -5,7 +5,7 @@ from api.src.models.category import Category, ExpenseCategory, RevenueCategory
 
 class Transaction(ABC):
     @abstractmethod
-    def __init__(self, t_id: int, user_id: int, name: str, description: str, value: float,
+    def __init__(self, t_id: int | None, user_id: int, name: str, description: str, value: float,
                  purchase_date: datetime, cat: Category):
         pass
 
@@ -22,7 +22,7 @@ class Transaction(ABC):
 
 class Expense(Transaction):
 
-    def __init__(self, t_id: int, user_id: int, name: str, description: str, value: float,
+    def __init__(self, t_id: int | None, user_id: int, name: str, description: str, value: float,
                  purchase_date: datetime, cat: ExpenseCategory):
         self._id = t_id
         self._user_id = user_id
@@ -46,7 +46,7 @@ class Expense(Transaction):
 
 
 class Revenue(Transaction):
-    def __init__(self, t_id: int, user_id: int, name: str, description: str, value: float,
+    def __init__(self, t_id: int | None, user_id: int, name: str, description: str, value: float,
                  purchase_date: datetime, cat: RevenueCategory):
         self._id = t_id
         self._user_id = user_id
@@ -65,5 +65,5 @@ class Revenue(Transaction):
         return self._user_id
 
     def __str__(self) -> str:
-        return f'Revenue(ID: {self._id}, Name: {self.name}, Description: {self.description}, Value: {self.value},'\
+        return f'Revenue(ID: {self._id}, Name: {self.name}, Description: {self.description}, Value: {self.value},' \
                f'Purchase Date: {self.purchase_date}, User ID: {self.user_id} Category ID: {self.cat.id})'
