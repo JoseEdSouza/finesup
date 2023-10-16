2fc9be53543bd2511f4fbebb384638ef4d73315c
Erick <erickferreiragaspar@gmail.com>
2023-10-16 16:53:55 -0300

    added fixed_transaction model

--- api/src/models/fixed_transaction.py
+++ api/src/models/fixed_transaction.py
@@ -1,8 +1,76 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
+from dataclasses import dataclass
 from api.src.utils.frequency import Frequency
 from api.src.models.category import Category, ExpenseCategory, RevenueCategory
 
-
+@dataclass
 class FixedTransaction(ABC):
-    pass
+    _id: int | None
+    user_id: int
+    name: str
+    description: str
+    value: float
+    cat: Category
+    purchase_date: datetime
+    limite_date: datetime
+    frequency: Frequency
+    
+    @abstractmethod
+    def __init__(self, ft_id: int | None, user_id: int, name: str, description: str, value: float, cat: Category,
+                purchase_date: datetime, limite_date: datetime, frequency: Frequency):
+        pass
+    
+    @property
+    @abstractmethod
+    def id(self) -> int:
+        pass
+    
+    @property
+    @abstractmethod
+    def user_id(self) -> int:
+        pass
+
+class FixedExpense(FixedTransaction):
+    
+    def __init__(self, ft_id: int | None, user_id: int, name: str, description: str, value: float, cat: ExpenseCategory,
+            purchase_date: datetime, limite_date: datetime, frequency: Frequency):
+        self._id=ft_id
+        self.user_id=user_id
+        self.name=name
+        self.description=description
+        self.value=value
+        self.cat=cat
+        self.purchase_date=purchase_date
+        self.limite_date=limite_date
+        self.frequency=frequency
+        
+    @property
+    def id(self) -> int:
+        return self._id
+    
+    @property
+    def user_id(self) -> int:
+        return self.user_id
+
+class FixedRevenue(FixedTransaction):
+    
+    def __init__(self, ft_id: int | None, user_id: int, name: str, description: str, value: float, cat: RevenueCategory,
+            purchase_date: datetime, limite_date: datetime, frequency: Frequency):
+        self._id=ft_id
+        self.user_id=user_id
+        self.name=name
+        self.description=description
+        self.value=value
+        self.cat=cat
+        self.purchase_date=purchase_date
+        self.limite_date=limite_date
+        self.frequency=frequency
+        
+    @property
+    def id(self) -> int:
+        return self._id
+    
+    @property
+    def user_id(self) -> int:
+        return self.user_id
