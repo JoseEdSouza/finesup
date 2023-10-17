2519b49d0bf919f93f7d852a1befdbc06c7d179b
Erick <erickferreiragaspar@gmail.com>
2023-10-16 22:34:22 -0300

    config electron window size

--- app/electron/main.ts
+++ app/electron/main.ts
@@ -21,10 +21,15 @@ const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 function createWindow() {
   win = new BrowserWindow({
     icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    width:800,
+    height:600,
+    minWidth:800,
+    minHeight:600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   })
+  win.setMenuBarVisibility(false)
 
   // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
