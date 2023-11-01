dec7c5d55bdca38cc583e47b3c8a14162bd8e6fb
PHS-7 <moreira.phs@alu.ufc.br>
2023-10-31 23:01:36 -0300

    category parameters refactoration

---
+++ api/src/routes/fixed_transaction_route.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from pydantic import BaseModel
+from typing import Optional
+from api.src.utils.route_meta import RouteMeta
+from api.src.models.fixed_transactionDAO import FixedExpenseDAOImp, FixedRevenueDAOImp
+from api.src.models.categoryDAO import ExpenseCategory, RevenueCategory
+from api.src.utils.frequency import Frequency
+
+
+class FixedExpenseRoute(metaclass=RouteMeta):
+    class FixedExpenseTransaction(BaseModel):
+        id: Optional[int] | None
+        user_id: int
+        name: str
+        description: str
+        value: float
+        cat: ExpenseCategory
+        purchase_date: float
+        limit_date: datetime
+        frequency: Frequency
+
+    DAO = FixedExpenseDAOImp()
+    Model = FixedExpenseTransaction
+    Key = ('id',)
+
+
+class FixedRevenueRoute(metaclass=RouteMeta):
+    class FixedRevenueTransaction(BaseModel):
+        id: Optional[int]
+        user_id: int
+        name: str
+        description: str
+        value: float
+        cat: int
+        purchase_date: datetime
+        limit_date: datetime
+        frequency: Frequency
+
+    DAO = FixedRevenueDAOImp()
+    Model = FixedRevenueTransaction
+    Key = ('id',)
---
+++ api/src/routes/transaction_route.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from pydantic import BaseModel
+from typing import Optional
+from api.src.utils.route_meta import RouteMeta
+from api.src.models.transactionDAO import ExpenseDAOImp, RevenueDAOImp
+from api.src.models.categoryDAO import ExpenseCategory, RevenueCategory
+
+
+class ExpenseRoute(metaclass=RouteMeta):
+    class ExpenseTransaction(BaseModel):
+        id: Optional[int]
+        user_id: int
+        name: str
+        description: str
+        value: float
+        purchase_date: datetime
+        cat: ExpenseCategory
+
+    DAO = ExpenseDAOImp()
+    Model = ExpenseTransaction
+    Key = ('id',)
+
+
+class RevenueRoute(metaclass=RouteMeta):
+    class RevenueTransaction(BaseModel):
+        id: Optional[int]
+        user_id: int
+        name: str
+        description: str
+        value: float
+        purchase_date: float
+        cat: int
+
+    DAO = RevenueDAOImp()
+    Model = RevenueTransaction
+    Key = ('id',)
