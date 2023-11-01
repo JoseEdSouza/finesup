17b6205c8b4b438ea5f60bd6e80bf12b1421e457
José Souza <joseedsouza.dev@gmail.com>
2023-10-31 21:27:03 -0300

    fixed route meta

--- api/src/utils/route_meta.py
+++ api/src/utils/route_meta.py
@@ -33,7 +33,7 @@ class RouteMeta(type):
                 @router.get(f'/{route_base_name}/{key_path}')
                 async def get_model(model_id: int = None, model_name: str = None,
                                     model_cat_id: int = None, model_user_id: int = None):
-                    args = list(filter(lambda x: x is not None, [model_user_id, model_id, model_name, model_cat_id]))
+                    args = list(filter(lambda x: x is not None, [model_id, model_user_id, model_name, model_cat_id]))
                     return dao.get(*args)
             if 'add' in methods:
                 url = f'/{route_base_name}/add' + ('' if len(key) == 1 else f'/{key_dict.get(key[0])}')
@@ -47,14 +47,14 @@ class RouteMeta(type):
                 @router.post(f'/{route_base_name}/update/{key_path}')
                 async def post_update_model(mod: model, model_id: int = None, model_name: str = None,
                                             model_cat_id: int = None, model_user_id: int = None):
-                    args = list(filter(lambda x: x is not None, [model_id, model_name, model_cat_id, model_user_id]))
+                    args = list(filter(lambda x: x is not None, [model_id, model_user_id, model_name, model_cat_id]))
                     dao.update(*args, mod)
                     return mod
             if 'remove' in methods:
                 @router.get(f'/{route_base_name}/remove/{key_path}')
                 async def get_remove_model(model_id: int = None, model_name: str = None,
                                            model_cat_id: int = None, model_user_id: int = None):
-                    args = list(filter(lambda x: x is not None, [model_id, model_name, model_cat_id, model_user_id]))
+                    args = list(filter(lambda x: x is not None, [model_id, model_user_id, model_name, model_cat_id]))
                     return dao.remove(*args)
             if 'get_all' in methods:
                 partial_key_path = f'{key_dict.get(key[0])}'
@@ -62,7 +62,7 @@ class RouteMeta(type):
                 @router.get(f'/{route_base_name}/all/{partial_key_path}')
                 async def get_all_model(model_id: int = None, model_name: str = None,
                                         model_cat_id: int = None, model_user_id: int = None):
-                    args = list(filter(lambda x: x is not None, [model_id, model_name, model_cat_id, model_user_id]))
+                    args = list(filter(lambda x: x is not None, [model_id, model_user_id, model_name, model_cat_id]))
                     return dao.get_all(*args)
         else:
             raise ValueError("Missing required attributes: 'Model', 'DAO', 'Key'")
