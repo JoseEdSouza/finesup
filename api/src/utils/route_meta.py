1060c001c58f301f60b9f3cd65787e717f049e66
Pedro Henrique Santos Moreira <moreira.phs@alu.ufc.bra>
2023-11-14 13:26:20 -0300

    route-meta refatored

--- api/src/utils/route_meta.py
+++ api/src/utils/route_meta.py
@@ -19,10 +19,10 @@ class RouteMeta(type):
             route_base_name = model.__name__.lower()
 
             key_dict = {
-                'id': 'model_id',
-                'name': 'model_name',
-                'cat_id': 'model_cat_id',
-                'user_id': 'model_user_id'
+                'id': 'id',
+                'name': 'm_name',
+                'cat_id': 'category',
+                'user_id': 'user'
             }
 
             key_path = f'{key_dict.get(key[0])}'
@@ -31,38 +31,38 @@ class RouteMeta(type):
 
             if 'get' in methods:
                 @router.get(f'/{route_base_name}/{key_path}')
-                async def get_model(model_id: int = None, model_name: str = None,
-                                    model_cat_id: int = None, model_user_id: int = None):
-                    args = list(filter(lambda x: x is not None, [model_id, model_user_id, model_name, model_cat_id]))
+                async def get(id: int = None, m_name: str = None,
+                              category: int = None, user: int = None):
+                    args = list(filter(lambda x: x is not None, [id, user, m_name, category]))
                     return dao.get(*args)
             if 'add' in methods:
                 url = f'/{route_base_name}/add' + ('' if len(key) == 1 else f'/{key_dict.get(key[0])}')
 
                 @router.post(url)
-                async def post_add_model(mod: model, model_id: int = None, model_user_id=None):
-                    args = list(filter(lambda x: x is not None, [model_id, model_user_id]))
+                async def post(mod: model, id: int = None, user=None):
+                    args = list(filter(lambda x: x is not None, [id, user]))
                     dao.add(*args, mod) if len(args) != 0 else dao.add(mod)
                     return mod
             if 'update' in methods:
-                @router.post(f'/{route_base_name}/update/{key_path}')
-                async def post_update_model(mod: model, model_id: int = None, model_name: str = None,
-                                            model_cat_id: int = None, model_user_id: int = None):
-                    args = list(filter(lambda x: x is not None, [model_id, model_user_id, model_name, model_cat_id]))
+                @router.put(f'/{route_base_name}/{key_path}')
+                async def put(mod: model, id: int = None, m_name: str = None,
+                              category: int = None, user: int = None):
+                    args = list(filter(lambda x: x is not None, [id, user, m_name, category]))
                     dao.update(*args, mod)
                     return mod
             if 'remove' in methods:
-                @router.get(f'/{route_base_name}/remove/{key_path}')
-                async def get_remove_model(model_id: int = None, model_name: str = None,
-                                           model_cat_id: int = None, model_user_id: int = None):
-                    args = list(filter(lambda x: x is not None, [model_id, model_user_id, model_name, model_cat_id]))
+                @router.delete(f'/{route_base_name}/{key_path}')
+                async def delete(id: int = None, m_name: str = None,
+                                 category: int = None, user: int = None):
+                    args = list(filter(lambda x: x is not None, [id, user, m_name, category]))
                     return dao.remove(*args)
             if 'get_all' in methods:
                 partial_key_path = f'{key_dict.get(key[0])}'
 
                 @router.get(f'/{route_base_name}/all/{partial_key_path}')
-                async def get_all_model(model_id: int = None, model_name: str = None,
-                                        model_cat_id: int = None, model_user_id: int = None):
-                    args = list(filter(lambda x: x is not None, [model_id, model_user_id, model_name, model_cat_id]))
+                async def get_all(id: int = None, m_name: str = None,
+                                  category: int = None, user: int = None):
+                    args = list(filter(lambda x: x is not None, [id, user, m_name, category]))
                     return dao.get_all(*args)
         else:
             raise ValueError("Missing required attributes: 'Model', 'DAO', 'Key'")
