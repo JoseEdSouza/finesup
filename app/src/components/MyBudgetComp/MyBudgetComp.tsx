2bbebf9331a4882d48952def6f7517c22e95c1ef
Ítalo Kauã <italo.kaua@alu.ufc.br>
2023-11-18 20:10:22 -0300

    adding div of percents

--- app/src/components/MyBudgetComp/MyBudgetComp.tsx
+++ app/src/components/MyBudgetComp/MyBudgetComp.tsx
@@ -1,10 +1,15 @@
 import AddButtonBudget from "../AddButton/AddButtonBudget"
+import TopicMyBudget from "../TopicMyBudget/TopicMyBudget"
 import "./MyBudgetComp.css"
 
 function MyBudgetComp(){
     return(
         <>
             <AddButtonBudget/>
+            <TopicMyBudget/>
+            <div id="divTitle">
+                <label id="totalBud"><strong>Orçamento total</strong></label>
+            </div>
         </>
     )
 }
