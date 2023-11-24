e4cf66890d3f4ca375e1d3821f4febe859d390c6
Erick <erickferreiragaspar@gmail.com>
2023-11-24 11:15:01 -0300

    add transactionCreateName component
    add transactionCreateDescription component

--- app/src/components/TransactionCreateContainer/TransactionCreateContainer.tsx
+++ app/src/components/TransactionCreateContainer/TransactionCreateContainer.tsx
@@ -3,6 +3,8 @@ import CreateButtons from "../CreateButtons/CreateButtons"
 import DisplayReveneExpense from "../DisplayReveneExpense/DisplayReveneExpense"
 import "./TransactionCreateContainer.css"
 import TransactionCreateCategory from "../TransactionCreateCategory/TransactionCreateCategory"
+import TransactionCreateDescription from "../TransactionCreateDescription/TransactionCreateDescription"
+import TransactionCreateName from "../TransactionCreateName/TransactionCreateName"
 
 function TransactionCreateContainer() {
     const [typeTransacttion, setTypeTransacttion] = useState(0)
@@ -10,7 +12,9 @@ function TransactionCreateContainer() {
     return (
         <div id="containerAux">
             <div id="transactionCreateconatiner">
+                <TransactionCreateName/>
                 <DisplayReveneExpense setTypeTransactions={setTypeTransacttion} bottom="61.71"/>
+                <TransactionCreateDescription/>
                 <TransactionCreateCategory categoryType={typeTransacttion}/>
                 <CreateButtons nameButton="Salvar" backTo="/2/transactionHistory"/>
             </div>
