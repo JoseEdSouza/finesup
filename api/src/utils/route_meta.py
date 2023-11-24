a1cd172cce8096b01458a04d3ada48a56fa226a1
José Souza <joseedsouza.dev@gmail.com>
2023-11-24 18:40:41 -0300

    fixed a bug in routemeta

--- api/src/utils/route_meta.py
+++ api/src/utils/route_meta.py
@@ -37,11 +37,13 @@ class RouteMeta(type):
                             dependencies=[Depends(Bearer())], tags=[tag])
                 async def get(id: int = None, m_name: str = None,
                               category: int = None, authorization: str = Header(None)):
-                    token = authorization.split(" ")[1]
-                    payload = Auth.decode(token)
-                    if payload is None:
-                        raise HTTPException(status_code=401, detail="Invalid token")
-                    user = payload['user_id']
+                    user = None
+                    if 'user_id' in key:
+                        token = authorization.split(" ")[1]
+                        payload = Auth.decode(token)
+                        if payload is None:
+                            raise HTTPException(status_code=401, detail="Invalid token")
+                        user = payload['user_id']
                     args = list(filter(lambda x: x is not None, [id, user, m_name, category]))
                     ret = dao.get(*args)
                     if ret is None:
@@ -66,11 +68,13 @@ class RouteMeta(type):
                             tags=[tag])
                 async def put(mod: model, id: int = None, m_name: str = None,
                               category: int = None, authorization: str = Header(None)):
-                    token = authorization.split(" ")[1]
-                    payload = Auth.decode(token)
-                    if payload is None:
-                        raise HTTPException(status_code=401, detail="Invalid token")
-                    user = payload['user_id']
+                    user = None
+                    if 'user_id' in key:
+                        token = authorization.split(" ")[1]
+                        payload = Auth.decode(token)
+                        if payload is None:
+                            raise HTTPException(status_code=401, detail="Invalid token")
+                        user = payload['user_id']
                     args = list(filter(lambda x: x is not None, [id, user, m_name, category]))
                     if dao.get(*args) is None:
                         raise HTTPException(status_code=404, detail="Item not found")
@@ -83,11 +87,13 @@ class RouteMeta(type):
                                dependencies=[Depends(Bearer())], tags=[tag])
                 async def delete(id: int = None, m_name: str = None,
                                  category: int = None, authorization: str = Header(None)):
-                    token = authorization.split(" ")[1]
-                    payload = Auth.decode(token)
-                    if payload is None:
-                        raise HTTPException(status_code=401, detail="Invalid token")
-                    user = payload['user_id']
+                    user = None
+                    if 'user_id' in key:
+                        token = authorization.split(" ")[1]
+                        payload = Auth.decode(token)
+                        if payload is None:
+                            raise HTTPException(status_code=401, detail="Invalid token")
+                        user = payload['user_id']
                     args = list(filter(lambda x: x is not None, [id, user, m_name, category]))
                     if dao.get(*args) is None:
                         raise HTTPException(status_code=404, detail="Item not found")
