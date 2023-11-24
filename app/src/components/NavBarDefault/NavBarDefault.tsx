5563d32f0545c12f536fd6d876c0948e2057bb4c
Ítalo Kauã <italo.kaua@alu.ufc.br>
2023-11-24 20:26:16 -0300

    Add back to Home in Navbar Default

--- app/src/components/NavBarDefault/NavBarDefault.tsx
+++ app/src/components/NavBarDefault/NavBarDefault.tsx
@@ -7,6 +7,7 @@ const NavBarDefault = (props:{name:string, backTo:string}) => {
         <div id="navBarDefault">
             <Link to={props.backTo === "1" ? "/calculators" : props.backTo === "2" ? "/1" : props.backTo === "3" ? "/3/mybudget" : props.backTo === "4" ? "/2/transactionHistory" : props.backTo === "5" ? "/3/budgetdetail" : "/"} id="linkBackIcon"><BackIcon/></Link>
             <label id="labelPageDefault"><strong>{props.name}</strong></label>
+            <Link to="/" ><label id="routeHomeD">Home</label></Link>
         </div>
     )
 }
