c90d75f429d2c58d97a3c11439ad91e4314353d6
Ítalo Kauã <italo.kaua@alu.ufc.br>
2023-11-26 00:25:19 -0300

    Add user icon
    Add exit button to user navbar

--- app/src/components/ElipseUser/ElipseUser.tsx
+++ app/src/components/ElipseUser/ElipseUser.tsx
@@ -3,7 +3,7 @@ import "./ElipseUser.css"
 function ElipseUser(){
     return(
         <div id="elipse">
-            
+            <img src="./icon_user.jpeg" className="LogoUser"/>
         </div>
     )
 }
--- app/src/components/NavBarUser/NavBarUser.tsx
+++ app/src/components/NavBarUser/NavBarUser.tsx
@@ -7,8 +7,7 @@ function NavBarUser(props:{name:string, backTo:string}){
         <div id="navBarDefault">
             <Link to={props.backTo === "1" ? "/calculators" : props.backTo === "2" ? "/1" : props.backTo === "3" ? "/3/mybudget" : props.backTo === "4" ? "/2/transactionHistory" : props.backTo === "5" ? "/3/budgetdetail" : "/home"} id="linkBackIcon"><BackIcon/></Link>
             <label id="labelPageDefault"><strong>{props.name}</strong></label>
-            <Link to="/home" ><label id="routeHomeD">Home</label></Link>
-
+            <Link to={props.backTo}><button id="outButton">sair</button></Link>
         </div>
     )
 }
