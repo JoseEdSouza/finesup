fecf2e7ba1381a2d87ebb305603d437128adffd3
Ítalo Kauã <italo.kaua@alu.ufc.br>
2023-11-28 16:28:41 -0300

    Add Graphics at Div of subcategories

--- app/src/components/DivCategoriesBud/DivCategoriesBud.tsx
+++ app/src/components/DivCategoriesBud/DivCategoriesBud.tsx
@@ -1,3 +1,4 @@
+import GraphicsDivBudget from "../GraphicsDivBudget/GraphicsDivBudget"
 import "./DivCategoriesBud.css"
 import { Link } from "react-router-dom"
 
@@ -6,12 +7,24 @@ function DivCategoriesBud(){
         <Link to="/3/budgetdetail">
             <div id="div1">
                 <label id="label1"><strong>Roupas</strong></label>
+                <img src="/icon_edit.svg" className="EditIcon"/>
+                <img src="/icon_delete.svg" className="DeleteIcon"/>   
+                <img src="/icon_check.svg" className="CheckIcon"/>
+                <GraphicsDivBudget GraphicValues={[80.3, 19.7]}/>
             </div>
             <div id="div2">
                 <label id="label2"><strong>Alimentação</strong></label>
+                <img src="/icon_edit.svg" className="EditIcon"/>
+                <img src="/icon_delete.svg" className="DeleteIcon"/>
+                <img src="/icon_check.svg" className="CheckIcon"/>
+                <GraphicsDivBudget GraphicValues={[63.7, 36.3]}/>
             </div>
             <div id="div3">
                 <label id="label3"><strong>Lazer</strong></label>
+                <img src="/icon_edit.svg" className="EditIcon"/>
+                <img src="/icon_delete.svg" className="DeleteIcon"/>
+                <img src="/icon_check.svg" className="CheckIcon"/>
+                <GraphicsDivBudget GraphicValues={[21.7, 78.3]}/>
             </div>
         </Link>
     )
---
+++ app/src/components/GraphicsDivBudget/GraphicsDivBudget.tsx
@@ -0,0 +1,30 @@
+import {Pie} from "react-chartjs-2"
+import { Chart, ArcElement, Tooltip, Legend } from "chart.js"
+import TopicMyBudget from "../TopicMyBudget/TopicMyBudget"
+Chart.register(ArcElement, Tooltip, Legend)
+
+function GraphicsDivBudget(props:{GraphicValues : Array<number>}){
+    const data = {
+        labels : ["teste 1", "teste2"], 
+        datasets :  [{
+            data : props.GraphicValues,
+            backgroundColor : ["green", "grey"]
+        }]
+    }
+    
+    const options = {
+        plugins : {
+            legend : {
+                display : false
+            }
+        }
+    }
+
+    return(
+        <div style={{position: "absolute", width: "3.75vw", top:"5.46vh", left:"4,3vw"}}>
+            <Pie data={data} style={{position: "absolute", width: "3.75vw", top: "5.46vhvh", left:"4.3vw", border:"none"}} options={options}/>
+        </div>
+    )
+}
+
+export default GraphicsDivBudget
