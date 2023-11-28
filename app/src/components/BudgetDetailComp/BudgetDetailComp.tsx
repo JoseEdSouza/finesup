28a53fb5d769b9700ee33c55cb310fc95c9bc222
Ítalo Kauã <italo.kaua@alu.ufc.br>
2023-11-28 14:48:50 -0300

    Add progress Bar at Budget Detail

--- app/src/components/BudgetDetailComp/BudgetDetailComp.tsx
+++ app/src/components/BudgetDetailComp/BudgetDetailComp.tsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom"
 import "./BudgetDetailComp.css"
 import Transaction from "../Transaction/Transaction"
+import ProgressBar from "../ProgressBar/ProgressBar"
 
-function BudgetDetailComp(){
+function BudgetDetailComp(props: { name: string; valueCurrent: number; valueMax: number }){
     return(
         <>
+            <div id="DivBar">
+                <ProgressBar
+                    valueCurrent={props.valueCurrent}
+                    valueMax={props.valueMax}
+                    progress={0}
+                    width={49.72}
+                    height={6.25}
+                    top={20.84}
+                    backgroundStyle="var(--secondaryColor)"
+                    labelStyle=""
+                />
+            </div>
+
             <div id="editDiv">
                 <Transaction transactionType={1} transactionValue={900} transactionName="Pizza" category="Alimentation" description="Descriçaõ Teste" date={new Date(14-12-2004)}/>
                 <Transaction transactionType={1} transactionValue={900} transactionName="Pizza" category="Alimentation" description="Descriçaõ Teste" date={new Date(14-12-2004)}/>
