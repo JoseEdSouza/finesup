83e935fad2f9f69a6b42898a541ccbb903669edc
Pedro Henrique Santos Moreira <moreiara.phs@alu.ufc.br>
2023-11-27 21:05:22 -0300

    created handler/controller of Budget

---
+++ app/src/handler/BudgetHandler.ts
@@ -0,0 +1,54 @@
+import Budget from "../models/Budget";
+import { Nullable } from "../types";
+
+interface BudgetHandler {
+    handle(budget: Budget): boolean;
+}
+
+
+abstract class BudgetBaseHandler implements BudgetHandler {
+    protected next: Nullable<BudgetBaseHandler> = null;
+    abstract handle(budget: Budget): boolean;
+    setNextHandler(next: BudgetBaseHandler): BudgetBaseHandler {
+        this.next = next;
+        return this.next;
+    }
+}
+
+class ValidateBudgetActualValue extends BudgetBaseHandler {
+    handle(budget: Budget): boolean {
+        if (budget.actualValue < 0) {
+            budget.actualValue = 0
+        }
+        if (this.next === null)
+            return true
+        return this.next.handle(budget)
+    }
+}
+
+class ValidateBudgetFinalValue extends BudgetBaseHandler {
+    handle(budget: Budget): boolean {
+        if (budget.finalValue < 1)
+            throw new Error('O Orçamento deve ter um valor maior que $1.00')
+        if (this.next === null)
+            return true
+        return this.next.handle(budget)
+    }
+}
+
+class CreateBudgetHandler extends BudgetBaseHandler {
+    handle(budget: Budget): boolean {
+        if (this.next === null)
+            return true
+        return this.next.handle(budget)
+    }
+}
+
+
+export type { BudgetHandler }
+export {
+    BudgetBaseHandler,
+    ValidateBudgetActualValue,
+    ValidateBudgetFinalValue,
+    CreateBudgetHandler
+}
---
+++ app/src/models/Category.ts
@@ -0,0 +1,19 @@
+//TODO - IMPLEMENTAR CERTO
+
+class ExpenseCategory {
+    constructor(
+        public name: string,
+        public color: string
+    ){}
+}
+class ReveneuCategory {
+    constructor(
+        public name: string,
+        public color: string
+    ){}
+}
+
+export {
+    ReveneuCategory,
+    ExpenseCategory
+}
