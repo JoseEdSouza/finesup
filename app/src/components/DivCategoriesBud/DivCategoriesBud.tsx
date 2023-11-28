b6316b9d22799e48fefb4e2aeb757a8905480c00
Ítalo Kauã <italo.kaua@alu.ufc.br>
2023-11-28 20:16:56 -0300

    Add dinamic budget

--- app/src/components/DivCategoriesBud/DivCategoriesBud.tsx
+++ app/src/components/DivCategoriesBud/DivCategoriesBud.tsx
@@ -1,69 +1,51 @@
-import GraphicsDivBudget from "../GraphicsDivBudget/GraphicsDivBudget"
-import "./DivCategoriesBud.css"
-import { Link } from "react-router-dom"
-import ProgressBar from "../ProgressBar/ProgressBar"
+import { useEffect, useRef, useState } from "react";
+import BudgetController from "../../controllers/BudgetController";
+import BudgetClass from "../../models/Budget";
+import { getCategoryByTypeAndId } from "../../models/Category";
+import Budget from "../Budget/Budget";
 
-function DivCategoriesBud(props: { name: string; valueCurrent: number; valueMax: number }){
+function DivCategoriesBud(){
+    const [budgetlist, setbudgetlist] = useState<JSX.Element[]>()
+    const controllerBudget = new BudgetController()
+    const effectRam = useRef(false)
+    const [alertbudget,setalertbudgetlist] = useState(false)
+
+    const Fetchdata = async () => {
+        try {
+            const auxiliar = await controllerBudget.getAll()
+            const listaBudget = auxiliar.map((e) => createBudget(e))
+            setbudgetlist(listaBudget)
+            setalertbudgetlist(true)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const createBudget = (budget: BudgetClass) => {
+        return(
+            <Budget name={getCategoryByTypeAndId(true,budget.categoryId)} valueCurrent={budget.actualValue} valueMax={budget.finalValue}/>
+        )
+    }
+
+    useEffect(() => {
+        if(!effectRam.current){
+            Fetchdata()
+            effectRam.current = true
+        }
+    },[])
+
     return(
-        <Link to="/3/budgetdetail">
-            <div id="div1">
-                <label id="label1"><strong>Roupas</strong></label>
-                <img src="/icon_edit.svg" className="EditIcon"/>
-                <img src="/icon_delete.svg" className="DeleteIcon"/>   
-                <img src="/icon_check.svg" className="CheckIcon"/>
-                <GraphicsDivBudget GraphicValues={[80.3, 19.7]}/>
-                <div className="DivBarB">
-                    <ProgressBar
-                        valueCurrent={props.valueCurrent}
-                        valueMax={props.valueMax}
-                        progress={0}
-                        width={21.875}
-                        height={2.83}
-                        top={13.52}
-                        backgroundStyle="white"
-                        labelStyle=""
-                    />
-                </div>
+        <>
+        
+            {
+                alertbudget ?
+
+            <div id="divCategorieBud">
+                {budgetlist}
             </div>
-            <div id="div2">
-                <label id="label2"><strong>Alimentação</strong></label>
-                <img src="/icon_edit.svg" className="EditIcon"/>
-                <img src="/icon_delete.svg" className="DeleteIcon"/>
-                <img src="/icon_check.svg" className="CheckIcon"/>
-                <GraphicsDivBudget GraphicValues={[63.7, 36.3]}/>
-                <div className="DivBarB">
-                    <ProgressBar
-                        valueCurrent={props.valueCurrent}
-                        valueMax={props.valueMax}
-                        progress={0}
-                        width={21.875}
-                        height={2.83}
-                        top={13.52}
-                        backgroundStyle="white"
-                        labelStyle=""
-                    />
-                </div>
-            </div>
-            <div id="div3">
-                <label id="label3"><strong>Lazer</strong></label>
-                <img src="/icon_edit.svg" className="EditIcon"/>
-                <img src="/icon_delete.svg" className="DeleteIcon"/>
-                <img src="/icon_check.svg" className="CheckIcon"/>
-                <GraphicsDivBudget GraphicValues={[21.7, 78.3]}/>
-                <div className="DivBarB">
-                    <ProgressBar
-                        valueCurrent={props.valueCurrent}
-                        valueMax={props.valueMax}
-                        progress={0}
-                        width={21.875}
-                        height={2.83}
-                        top={13.52}
-                        backgroundStyle="white"
-                        labelStyle=""
-                    />
-                </div>
-            </div>
-        </Link>
+            : <label id="alertMessage">Não possui budget</label>
+            }
+        </>
     )
 }
 
--- app/src/models/Category.ts
+++ app/src/models/Category.ts
@@ -3,33 +3,33 @@ export class ExpenseCategory {
         public readonly name: string,
         public readonly color: string,
         public readonly id: number
-    ){}
+    ) { }
 }
 export class RevenueCategory {
     constructor(
         public readonly name: string,
         public readonly color: string,
         public readonly id: number
-    ){} 
+    ) { }
 }
 
-export  const ExpenseCategories = {  
+export const ExpenseCategories = {
     ALIMENTATION: new ExpenseCategory("Alimentação", "#CC2E43", 1),
     LEISURE: new ExpenseCategory("Lazer", "#F7FFA1", 2),
-    CLOTHES: new ExpenseCategory("Roupas", "#70B6F6", 3), 
-    MARKET: new ExpenseCategory("Mercado","#5AF961", 4),
-    TRANSPORT: new ExpenseCategory("Transporte","#A2A0FA", 5) ,
-    HEALTH: new ExpenseCategory("Saúde","#FF6928", 6),
-    TRAVEL: new ExpenseCategory("Viagem","#C728F", 7),
-    PETS: new ExpenseCategory("Pets","#2D28F", 8),
-    HOME: new ExpenseCategory("Moradia","#28B2FF", 9), 
-    ACCOUNTS: new ExpenseCategory("Contas","#28FF64", 10), 
-    DONATION: new ExpenseCategory("Doações","#AD3D3D", 11), 
-    EDUCATION: new ExpenseCategory("Educação","#4D91B8", 12), 
-    TAX: new ExpenseCategory("Imposto","#FFEA28", 13), 
-    INVESTMENT: new ExpenseCategory("Investimento Saída","#FF6928", 14), 
-    EXCHANGE: new ExpenseCategory("Câmbio Saída","#0FBB00", 15),
-    EXITOTHERS: new ExpenseCategory("Outras Entrada","#28FFBFs", 16) 
+    CLOTHES: new ExpenseCategory("Roupas", "#70B6F6", 3),
+    MARKET: new ExpenseCategory("Mercado", "#5AF961", 4),
+    TRANSPORT: new ExpenseCategory("Transporte", "#A2A0FA", 5),
+    HEALTH: new ExpenseCategory("Saúde", "#FF6928", 6),
+    TRAVEL: new ExpenseCategory("Viagem", "#C728F", 7),
+    PETS: new ExpenseCategory("Pets", "#2D28F", 8),
+    HOME: new ExpenseCategory("Moradia", "#28B2FF", 9),
+    ACCOUNTS: new ExpenseCategory("Contas", "#28FF64", 10),
+    DONATION: new ExpenseCategory("Doações", "#AD3D3D", 11),
+    EDUCATION: new ExpenseCategory("Educação", "#4D91B8", 12),
+    TAX: new ExpenseCategory("Imposto", "#FFEA28", 13),
+    INVESTMENT: new ExpenseCategory("Investimento Saída", "#FF6928", 14),
+    EXCHANGE: new ExpenseCategory("Câmbio Saída", "#0FBB00", 15),
+    EXITOTHERS: new ExpenseCategory("Outras Entrada", "#28FFBFs", 16)
 }
 export const RevenueCategories = {
     CASHBACK: new RevenueCategory("CashBack", "#CC2E2E", 1),
@@ -42,4 +42,21 @@ export const RevenueCategories = {
     INVESTMENT: new RevenueCategory("Investiemnto Entrada", "#FF6928", 8),
     EXCHANGE: new RevenueCategory("Câmbio Entrada", "#0FBB00", 9),
     ENTRYOTHERS: new RevenueCategory("Outras Entradas", "#A6EB15", 10)
+}
+
+export const getCategoryByTypeAndId = (type: boolean, id: number) => {
+    let categories;
+    if (type) {
+        categories = ExpenseCategories;
+    } else {
+        categories = RevenueCategories;
+    }
+
+    for (let category in categories) {
+        if (categories[category].id === id) {
+            return categories[category];
+        }
+    }
+
+    return null;
 }
