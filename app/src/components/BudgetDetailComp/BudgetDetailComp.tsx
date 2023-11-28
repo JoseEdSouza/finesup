b0f3821708727c6a37cfd774a29ee089f13807e6
Ítalo Kauã <italo.kaua@alu.ufc.br>
2023-11-28 14:10:51 -0300

    Add div catergorie at Budget Detail Screen

--- app/src/components/BudgetDetailComp/BudgetDetailComp.tsx
+++ app/src/components/BudgetDetailComp/BudgetDetailComp.tsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom"
 import "./BudgetDetailComp.css"
+import Transaction from "../Transaction/Transaction"
 
 function BudgetDetailComp(){
     return(
         <>
-            <Link to = "/3/budgetedit">
-                <div id="editDiv">
-                    <img src="/icon_edit.svg" className="EditBud"/>
-                </div>
-            </Link>
+            <div id="editDiv">
+                <Transaction transactionType={1} transactionValue={900} transactionName="Pizza" category="Alimentation" description="Descriçaõ Teste" date={new Date(14-12-2004)}/>
+            </div>
+
+            <div id="editDiv2">
+                <Link to = "/3/budgetedit"><img src="/icon_edit.svg" className="EditBud"/> </Link>
+            </div>
         </>
     )
 }
