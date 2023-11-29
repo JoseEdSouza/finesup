974c69d07e3aef9082dfb3d0b2489ca976af776d
Pedro Henrique Santos Moreira <moreiara.phs@alu.ufc.br>
2023-11-29 01:31:41 -0300

    delete account implemented

--- app/src/components/DeleteAccount/DeleteAccount.tsx
+++ app/src/components/DeleteAccount/DeleteAccount.tsx
@@ -1,16 +1,23 @@
 import { useNavigate } from "react-router-dom"
 import "./DeleteAccount.css"
+import UserController from "../../controllers/UserController"
 
 function DeleteAccount(props: { deleteAccount: (confirm: boolean) => void }) {
     const navigate = useNavigate()
-    
+
+    const handleDeleteAccount = async () => {
+        await UserController.deleteAccount();
+        props.deleteAccount(true);
+        navigate('/'); // navigate to home page or login page after account deletion
+    }
+
     return (
         <div style={{ position: "absolute", width: "100vw", height: "130vh", left: "50%", transform: "translateX(-50%)", zIndex: "1" }}>
             <div id="deleteAccount">
                 <img src="/icon_cancel.svg" className="cancelIcon" onClick={() => props.deleteAccount(false)} />
                 <label id="warining"><strong>Tem certeza que deseja excluir sua conta?</strong></label>
                 <label id="warining2">Essa ação é irreversível, quando concordar em excluir sua conta você  nunca mais poderá acessá-la de novo.</label>
-                <button id="confirm">Sim, quero excluir</button>
+                <button id="confirm" onClick={handleDeleteAccount}>Sim, quero excluir</button>
                 <button id="regreat" onClick={() => props.deleteAccount(false)}>Não quero excluir</button>
             </div>
         </div>
